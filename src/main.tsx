1a55d0c1690fcaddb9b17f88ec6e8b34d87a77bd
KamanDShrestha <109749519+KamanDShrestha@users.noreply.github.com>
2023-07-13 11:32:34 +0545

    Setting up React Router for Home page and Game Detail

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
-import { App } from './App';
+
 import './index.css';
 import theme from './theme';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { RouterProvider } from 'react-router-dom';
+import router from './router';
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
@@ -14,7 +16,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <QueryClientProvider client={queryClient}>
       <ChakraProvider theme={theme}>
         <ColorModeScript initialColorMode={theme.config.intialColorMode} />
-        <App />
+        <RouterProvider router={router} />
         <ReactQueryDevtools />
       </ChakraProvider>
     </QueryClientProvider>
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,44 @@
+import { Grid, GridItem, Show, HStack } from '@chakra-ui/react';
+import { Box } from '@chakra-ui/react';
+import React from 'react';
+import GameGrid from '../components/GameGrid';
+import GameHeading from '../components/GameHeading';
+import GenreList from '../components/GenreList';
+import { NavBar } from '../components/NavBar';
+import OrderBy from '../components/OrderBy';
+import PlatformSelection from '../components/PlatformSelection';
+
+const HomePage = () => {
+  return (
+    <>
+      <Grid
+        templateAreas={{
+          base: `"main"`,
+          lg: ` "aside main"`,
+        }}
+        templateColumns={{
+          base: '1fr',
+          lg: '200px 1fr',
+        }}
+      >
+        <Show above='lg'>
+          <GridItem area='aside' paddingX={4}>
+            <GenreList />
+          </GridItem>
+        </Show>
+        <GridItem area='main' paddingX={4}>
+          <Box paddingX={5}>
+            <GameHeading />
+            <HStack spacing={4} marginTop={5}>
+              <PlatformSelection />
+              <OrderBy />
+            </HStack>
+          </Box>
+          <GameGrid />
+        </GridItem>
+      </Grid>
+    </>
+  );
+};
+
+export default HomePage;
